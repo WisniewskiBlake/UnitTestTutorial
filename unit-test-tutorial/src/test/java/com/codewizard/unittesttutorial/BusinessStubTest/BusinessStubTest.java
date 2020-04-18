3bb23bbdd23ffd708bc67b2d7b692044afb8429b
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-17 23:08:41 -0400

    passed test

---
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessStubTest/BusinessStubTest.java
@@ -0,0 +1,36 @@
+package com.codewizard.unittesttutorial.BusinessStubTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.codewizard.unittesttutorial.Business.SomeBusinessImpl;
+import com.codewizard.unittesttutorial.Data.SomeDataService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SomeDataServiceStub implements SomeDataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        // TODO Auto-generated method stub
+        return new int[] {1, 2, 3};
+    }
+    
+}
+
+@SpringBootTest
+class BusinessStubTest {
+
+	@Test
+	void calcSumUsingDataService() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        business.setSomeDataService(new SomeDataServiceStub());
+        int actualResult = business.calcSumUsingDataService();
+        int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+    }
+         
+
+    
+
+}
--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/SomeBusinessTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/SomeBusinessTest.java
@@ -10,21 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SomeBusinessTest {
 
-	@Test
-	void calcSum() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        int actualResult = business.calculateSum(new int[] {1,2,3});
-        int expectedResult = 6;
-        assertEquals(expectedResult, actualResult);
-    }
+	// @Test
+	// void calcSum() {
+    //     SomeBusinessImpl business = new SomeBusinessImpl();
+    //     int actualResult = business.calculateSum(new int[] {1,2,3});
+    //     int expectedResult = 6;
+    //     assertEquals(expectedResult, actualResult);
+    // }
          
 
-    @Test
-	void calcSumEmpty() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-        int actualResult = business.calculateSum(new int[] {});
-        int expectedResult = 0;
-        assertEquals(expectedResult, actualResult);
-	}
+    // @Test
+	// void calcSumEmpty() {
+    //     SomeBusinessImpl business = new SomeBusinessImpl();
+    //     int actualResult = business.calculateSum(new int[] {});
+    //     int expectedResult = 0;
+    //     assertEquals(expectedResult, actualResult);
+	// }
 
 }
