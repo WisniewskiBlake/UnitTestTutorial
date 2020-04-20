58c0c217d6626fcf47d5e30ef817d36b03c01d78
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-20 03:21:08 -0400

    working unit test

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/ListMockTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/ListMockTest.java
@@ -1,5 +1,8 @@
 package com.codewizard.unittesttutorial.BusinessTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,8 @@ public class ListMockTest {
     @Test
     public void test() {
         List mock = Mockito.mock(List.class);
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
     }
 
 }
