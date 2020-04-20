18f943c39fd7f2b0cd3d015295e75d6fbc8a8740
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-19 20:33:50 -0400

    adding mockito

---
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/BusinessMockTest.java
@@ -0,0 +1,31 @@
+package com.codewizard.unittesttutorial.BusinessTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import com.codewizard.unittesttutorial.Business.SomeBusinessImpl;
+import com.codewizard.unittesttutorial.Data.SomeDataService;
+
+import org.junit.jupiter.api.Test;
+
+public class BusinessMockTest {
+    @Test
+	void calcSumUsingDataService_oneValue() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        SomeDataService dataServiceMock = mock(SomeDataService.class);
+
+        business.setSomeDataService(new SomeDataServiceStub());
+        int actualResult = business.calcSumUsingDataService();
+        int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+    }
+    
+    @Test
+	void calcSumUsingDataService_empty() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        business.setSomeDataService(new SomeDataServiceStub());
+        int actualResult = business.calcSumUsingDataService();
+        int expectedResult = 0;
+        assertEquals(expectedResult, actualResult);
+    }
+}
