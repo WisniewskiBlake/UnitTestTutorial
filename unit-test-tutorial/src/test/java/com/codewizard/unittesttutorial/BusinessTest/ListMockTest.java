946b7954ce3fef01fd951261879c0d0d074ee8c3
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-20 03:47:46 -0400

    unit test w generic params

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/ListMockTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/ListMockTest.java
@@ -1,6 +1,7 @@
 package com.codewizard.unittesttutorial.BusinessTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -28,6 +29,13 @@ public class ListMockTest {
     @Test
     public void returnWithParams() {        
         when(mock.get(0)).thenReturn("Code Wizard");
+        assertEquals("Code Wizard", mock.get(0));
+        assertEquals(null, mock.get(1));         
+    }
+
+    @Test
+    public void returnWithGenericParams() {        
+        when(mock.get(anyInt())).thenReturn("Code Wizard");
         assertEquals("Code Wizard", mock.get(0));
         assertEquals(null, mock.get(1));         
     }
