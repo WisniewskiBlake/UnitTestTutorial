2a869537702a401378a86a68a12f809d52030e2a
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-19 20:37:11 -0400

    mockito example

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/BusinessMockTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/BusinessMockTest.java
@@ -2,6 +2,7 @@ package com.codewizard.unittesttutorial.BusinessTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.codewizard.unittesttutorial.Business.SomeBusinessImpl;
 import com.codewizard.unittesttutorial.Data.SomeDataService;
@@ -12,9 +13,11 @@ public class BusinessMockTest {
     @Test
 	void calcSumUsingDataService_oneValue() {
         SomeBusinessImpl business = new SomeBusinessImpl();
+        //mock will return retrieveAllData new int[] {1, 2, 3}
         SomeDataService dataServiceMock = mock(SomeDataService.class);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1, 2, 3});
 
-        business.setSomeDataService(new SomeDataServiceStub());
+        business.setSomeDataService(dataServiceMock);
         int actualResult = business.calcSumUsingDataService();
         int expectedResult = 6;
         assertEquals(expectedResult, actualResult);
