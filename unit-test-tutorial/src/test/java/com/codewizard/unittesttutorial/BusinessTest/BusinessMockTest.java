646425a2a3a52b0604370541a8e48894d3acced4
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-19 21:59:11 -0400

    using run with

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/BusinessMockTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/BusinessMockTest.java
@@ -1,31 +1,32 @@
 package com.codewizard.unittesttutorial.BusinessTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 
 import com.codewizard.unittesttutorial.Business.SomeBusinessImpl;
 import com.codewizard.unittesttutorial.Data.SomeDataService;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class BusinessMockTest {
 
+    @InjectMocks    
     SomeBusinessImpl business = new SomeBusinessImpl();
+
     //mock will return retrieveAllData new int[] {1, 2, 3}
-    SomeDataService dataServiceMock = mock(SomeDataService.class);
-
-    @BeforeAll
-    public void before() {
-        business.setSomeDataService(dataServiceMock);
-    }
-
+    @Mock
+    SomeDataService dataServiceMock;
+    
     @Test
 	void calcSumUsingDataService_basic() {      
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1, 2, 3});
-
-        before();
+        
         int actualResult = business.calcSumUsingDataService();
         int expectedResult = 6;
         assertEquals(expectedResult, actualResult);
@@ -34,8 +35,7 @@ public class BusinessMockTest {
     @Test
 	void calcSumUsingDataService_empty() {        
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { });
-
-        before();
+        
         int actualResult = business.calcSumUsingDataService();        
         assertEquals(0, actualResult);
     }
@@ -43,8 +43,7 @@ public class BusinessMockTest {
     @Test
 	void calcSumUsingDataService_oneValue() {        
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {5});
-
-        before();
+        
         int actualResult = business.calcSumUsingDataService();
         int expectedResult = 5;
         assertEquals(expectedResult, actualResult);
