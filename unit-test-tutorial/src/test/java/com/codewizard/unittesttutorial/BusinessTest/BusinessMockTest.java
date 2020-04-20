b648daedb2aa77bb2fe3b6d5ea1f5eda4cdedf11
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-19 20:43:58 -0400

    exercise solution

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/BusinessMockTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/BusinessMockTest.java
@@ -28,24 +28,24 @@ public class BusinessMockTest {
         SomeBusinessImpl business = new SomeBusinessImpl();
         //mock will return retrieveAllData new int[] {1, 2, 3}
         SomeDataService dataServiceMock = mock(SomeDataService.class);
-        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1, 2, 3});
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { });
 
         business.setSomeDataService(dataServiceMock);
         int actualResult = business.calcSumUsingDataService();
-        int expectedResult = 6;
+        int expectedResult = 0;
         assertEquals(expectedResult, actualResult);
     }
 
     @Test
-	void calcSumUsingDataService_boneValue() {
+	void calcSumUsingDataService_oneValue() {
         SomeBusinessImpl business = new SomeBusinessImpl();
         //mock will return retrieveAllData new int[] {1, 2, 3}
         SomeDataService dataServiceMock = mock(SomeDataService.class);
-        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1, 2, 3});
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {2});
 
         business.setSomeDataService(dataServiceMock);
         int actualResult = business.calcSumUsingDataService();
-        int expectedResult = 6;
+        int expectedResult = 2;
         assertEquals(expectedResult, actualResult);
     }
 }
