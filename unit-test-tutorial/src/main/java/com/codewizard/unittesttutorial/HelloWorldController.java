3c962db24457244248d1ebe5389b48b0215cce52
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-22 01:24:03 -0400

    testing

--- unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/HelloWorldController.java
+++ unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/HelloWorldController.java
@@ -1,8 +1,14 @@
 package com.codewizard.unittesttutorial;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }    
 
 }
