08419671aac338e4eb42e76fc502336cf14a5e19
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-23 04:56:32 -0400

    mockMvc test

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/HelloWorldControllerTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/HelloWorldControllerTest.java
@@ -4,16 +4,31 @@ import com.codewizard.unittesttutorial.HelloWorldController;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(HelloWorldController.class)
 public class HelloWorldControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     public void helloWorld_basic() {
+
+        RequestBuilder request = MockMvcRequestBuilders
+            .get("/hello-world")
+            .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request);
+        
+
         //calling /hello-world
         //verifying "Hello World"
     }
