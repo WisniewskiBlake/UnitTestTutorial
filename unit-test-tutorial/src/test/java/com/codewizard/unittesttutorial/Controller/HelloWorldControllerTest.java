1f4438ff3c4d53ad233add3bc8f3641d80ddb482
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-23 11:24:12 -0400

    added status check

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/HelloWorldControllerTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/HelloWorldControllerTest.java
@@ -1,21 +1,24 @@
 package com.codewizard.unittesttutorial.Controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.regex.MatchResult;
 
 import com.codewizard.unittesttutorial.HelloWorldController;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.engine.TestExecutionResult.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.StatusAssertions;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(HelloWorldController.class)
@@ -27,10 +30,14 @@ public class HelloWorldControllerTest {
     @Test
     public void helloWorld_basic() throws Exception {
         //calling GET /hello-world application/json
-        RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(request).andReturn();
+        RequestBuilder request = MockMvcRequestBuilders.get("/hello-world")
+            .accept(MediaType.APPLICATION_JSON);
+        ResultMatcher ok = MockMvcResultMatchers.status()
+                                            .isOk();
+        MvcResult result = (MvcResult) mockMvc.perform(request)
+            .andExpect(ok)
+            .andReturn();
         
-
         
         //verifying "Hello World"
         assertEquals("Hello World", result.getResponse().getContentAsString());
