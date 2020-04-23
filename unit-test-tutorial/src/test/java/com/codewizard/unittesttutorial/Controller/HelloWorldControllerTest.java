7b704b52ee78ed2a283722238a7eef7c099f2ba6
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-23 04:51:45 -0400

    controller test method

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/HelloWorldControllerTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/HelloWorldControllerTest.java
@@ -2,6 +2,7 @@ package com.codewizard.unittesttutorial.Controller;
 
 import com.codewizard.unittesttutorial.HelloWorldController;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -10,5 +11,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(HelloWorldController.class)
 public class HelloWorldControllerTest {
+
+    @Test
+    public void helloWorld_basic() {
+        //calling /hello-world
+        //verifying "Hello World"
+    }
 
 }
