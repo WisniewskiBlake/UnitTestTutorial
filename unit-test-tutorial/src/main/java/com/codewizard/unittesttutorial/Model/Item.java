bfb0cd183d472d116175628fb2878227f5db22e2
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-23 20:48:08 -0400

    getters setter and tostring

--- unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/Model/Item.java
+++ unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/Model/Item.java
@@ -14,4 +14,59 @@ public class Item {
         this.quantity = quantity;
     }
 
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String toString() {
+        return String.format("Item[%d, %s, %d, %d]", id, name, price, quantity);
+    }
+
+
+
 }
