c4fffff4318f20bc557b811818079ddb6952f477
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-26 18:00:54 -0400

    test change

--- unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/ItemController.java
+++ unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/ItemController.java
@@ -2,11 +2,15 @@ package com.codewizard.unittesttutorial;
 
 import com.codewizard.unittesttutorial.Model.Item;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
+
+    @Autowired
+    ItemBusinessService businessService;
 
     @GetMapping("/dummy-item")
     public Item dummyItem() {
@@ -15,7 +19,7 @@ public class ItemController {
     
     @GetMapping("/item-from-business-service")
     public Item itemFromBusinessService() {
-        return new Item(1, "Ball", 10, 100);
+        return businessService.retreiveHardcodedItem();
     }
 
 }
