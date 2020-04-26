9bf390341fb825679b6afb01710ba21eefff2cd6
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-26 18:11:12 -0400

    business service class

---
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Business/ItemBusinessService.java
@@ -0,0 +1,11 @@
+package com.codewizard.unittesttutorial.Business;
+
+import com.codewizard.unittesttutorial.Model.Item;
+
+public class ItemBusinessService {
+
+    public Item retreiveHardcodedItem() {
+        return new Item(1, "Ball", 10, 100);
+    }
+
+}
