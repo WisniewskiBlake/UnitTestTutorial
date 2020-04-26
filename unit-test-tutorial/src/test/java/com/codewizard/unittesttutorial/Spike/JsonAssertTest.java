6ebe774d907881aea5885c2d8bcb175188bee9fc
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-25 20:50:25 -0400

    test data

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Spike/JsonAssertTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Spike/JsonAssertTest.java
@@ -1,5 +1,14 @@
 package com.codewizard.unittesttutorial.Spike;
 
+import org.junit.jupiter.api.Test;
+
 public class JsonAssertTest {
+
+    String str = "{\"id\":1,\"name\":\"Ball\",\"price\":10}";
+
+    @Test
+    public void jsonAssert() {
+
+    }
 
 }
