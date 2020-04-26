ac339d883a3b2cc53a4254f7090b7ff7fbf45681
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-25 20:52:05 -0400

    assert equal statement

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Spike/JsonAssertTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Spike/JsonAssertTest.java
@@ -1,14 +1,15 @@
 package com.codewizard.unittesttutorial.Spike;
 
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 public class JsonAssertTest {
 
-    String str = "{\"id\":1,\"name\":\"Ball\",\"price\":10}";
+    String actualResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
 
     @Test
     public void jsonAssert() {
-
+        JSONAssert.assertEquals(expected, actual, true);
     }
 
 }
