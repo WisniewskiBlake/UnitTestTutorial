6999c61dc2293ad3a75a5d1bdc119d08758c108a
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-26 20:24:58 -0400

    success test

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/ItemControllerTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Controller/ItemControllerTest.java
@@ -1,16 +1,21 @@
 package com.codewizard.unittesttutorial.Controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import java.util.regex.MatchResult;
 
 import com.codewizard.unittesttutorial.HelloWorldController;
 import com.codewizard.unittesttutorial.ItemController;
+import com.codewizard.unittesttutorial.Business.ItemBusinessService;
+import com.codewizard.unittesttutorial.Model.Item;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.engine.TestExecutionResult.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.StatusAssertions;
@@ -25,12 +30,19 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @WebMvcTest(ItemController.class)
 public class ItemControllerTest {
 
+    @MockBean
+    private ItemBusinessService businessService;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void itemFromBusinessService_basic() throws Exception {
-        //calling GET /hello-world application/json
+        
+        when(businessService.retreiveHardcodedItem()).thenReturn(
+                new Item(2,"Item2",10,10)
+        );
+
         RequestBuilder request = MockMvcRequestBuilders.get("/item-from-business-service")
             .accept(MediaType.APPLICATION_JSON);
         ResultMatcher ok = MockMvcResultMatchers.status()
@@ -38,7 +50,7 @@ public class ItemControllerTest {
         // ResultMatcher content = MockMvcResultMatchers.content()
         //     .json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}");
         ResultMatcher content = MockMvcResultMatchers.content()
-            .json("{id:1,name:Ball,price:10}");
+            .json("{id:2,name:Item2,price:10}");
         MvcResult result = (MvcResult) mockMvc.perform(request)
             .andExpect(ok)
             .andExpect(content)
