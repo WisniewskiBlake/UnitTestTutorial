d4fb2be3d1350474469ce44f8203d89a9edbf851
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-29 03:26:20 -0400

    created Mock

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/ItemBusinessServiceTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/BusinessTest/ItemBusinessServiceTest.java
@@ -1,5 +1,20 @@
 package com.codewizard.unittesttutorial.BusinessTest;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.codewizard.unittesttutorial.Business.ItemBusinessService;
+
+@ExtendWith(MockitoJUnitRunner.class)
 public class ItemBusinessServiceTest {
+
+    @InjectMocks
+    ItemBusinessService business;
+
+    @Mock
+    SomeDataService dataServiceMock;
+
+    
+    
 
 }
