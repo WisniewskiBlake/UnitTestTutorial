bd821aac472024a8aaf54cc84da2bbbd0d43c2c8
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-29 20:04:30 -0400

    added imports

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Data/ItemRepositoryTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Data/ItemRepositoryTest.java
@@ -1,5 +1,13 @@
 package com.codewizard.unittesttutorial.Data;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 public class ItemRepositoryTest {
 
+
+    
 }
