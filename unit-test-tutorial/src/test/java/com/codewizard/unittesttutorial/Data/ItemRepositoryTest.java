7acf9affb4ab2fe79d1482b0dffb8ca188d3e034
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-29 20:05:15 -0400

    added test method

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Data/ItemRepositoryTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Data/ItemRepositoryTest.java
@@ -1,13 +1,21 @@
 package com.codewizard.unittesttutorial.Data;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest
 public class ItemRepositoryTest {
 
+    @Autowired
+    private ItemRepository repository;
 
-    
+    @Test
+    public void testFindAll() {
+        
+    }
+
 }
