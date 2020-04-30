75880d29410ba341711bec1c14bc36a45eab2918
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-29 20:06:43 -0400

    complete test method

--- unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Data/ItemRepositoryTest.java
+++ unit-test-tutorial/src/test/java/com/codewizard/unittesttutorial/Data/ItemRepositoryTest.java
@@ -1,5 +1,11 @@
 package com.codewizard.unittesttutorial.Data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import com.codewizard.unittesttutorial.Model.Item;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,7 +21,8 @@ public class ItemRepositoryTest {
 
     @Test
     public void testFindAll() {
-        
+        List<Item> items = repository.findAll();
+        assertEquals(3, items.size());
     }
 
 }
