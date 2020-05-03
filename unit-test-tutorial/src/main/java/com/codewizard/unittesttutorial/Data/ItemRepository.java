c9433f53c8500e983fe15f78e44c4d34d9f32124
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-02 23:01:23 -0400

    created retrieve all method

--- unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/Data/ItemRepository.java
+++ unit-test-tutorial/src/main/java/com/codewizard/unittesttutorial/Data/ItemRepository.java
@@ -5,5 +5,6 @@ import com.codewizard.unittesttutorial.Model.Item;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
 public interface ItemRepository extends JpaRepositoryImplementation<Item,Integer> {
-
+    public static void retrieveAllData() {
+    }
 }
